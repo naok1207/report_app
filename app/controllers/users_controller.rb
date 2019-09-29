cbc6fc8881c50aacd6c27b8db7843450713fac3b
zenos <zenos.game.anime.all.vo1@gmail.com>
2019-09-29 18:11:12 +0900

    some change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @user = User.find(current_user.id)
@@ -54,7 +55,13 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
