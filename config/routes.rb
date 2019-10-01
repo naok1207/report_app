2140b2c1c59a0e99e1c82f298a615c877e71fddf
zenos <zenos.game.anime.all.vo1@gmail.com>
2019-10-01 23:44:47 +0900

    add git ignore .env

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root   'users#index'
 
   get   '/profile', to: 'profiles#edit'
@@ -19,4 +21,5 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
